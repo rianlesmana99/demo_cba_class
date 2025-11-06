13b06810152459547dced2e340d9f033af9265f1
rianlesmana <rian.lesmana9999@gmail.com>
2025-11-06 12:31:57 +0700

    Kamis, 06 November 2025

---
+++ Demo_1 (Firdha)/js/login.js
@@ -0,0 +1,12 @@
+console.log("Ini working!!!!");
+
+// Single Line Comment
+
+/**
+ * Multi line comment
+ * Variable: sebagai tempat untuk penyimpanan data
+ * Data Type: String, Number, Float, BigInt, Boolean, undefined, null
+ * DOM (Document Object Model) & Event
+ * Conditional Statements
+ * 
+ */
